1931dea9b2f897d99be21ec199001f99278cfc6f
gunners <chakagae123@naver.com>
2024-07-23 18:43:30 +0900

    * 섹션 1. URL 링크

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -73,6 +73,13 @@ public class BasicController {
 
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+
+        return "basic/link";
+    }
 
     @Data
     static class User {
